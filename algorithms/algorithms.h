c84564cabfc43ebd8df48140283a7aef8d91ca13
linich <linich.maksim@gmail.com>
2024-04-24 16:09:49 +0300

    Add test for isEmpty and top

---
+++ algorithms/algorithms.h
@@ -0,0 +1,18 @@
+//
+//  algorithms.h
+//  algorithms
+//
+//  Created by Maksim Linich on 24.04.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for algorithms.
+FOUNDATION_EXPORT double algorithmsVersionNumber;
+
+//! Project version string for algorithms.
+FOUNDATION_EXPORT const unsigned char algorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <algorithms/PublicHeader.h>
+
+
---
+++ algorithmsTests/HeapTests.swift
@@ -0,0 +1,38 @@
+//
+//  algorithmsTests.swift
+//  algorithmsTests
+//
+//  Created by Maksim Linich on 24.04.24.
+//
+
+import XCTest
+@testable import algorithms
+
+struct Heap<T> {
+
+    public var isEmpty: Bool {
+        return true
+    }
+    
+    public var top: T? {
+        return nil
+    }
+}
+
+final class algorithmsTests: XCTestCase {
+
+    func test_isEmpty_returnsTrueOnInitialize() {
+        let sut: Heap<Int> = createSUT()
+        XCTAssertTrue(sut.isEmpty)
+    }
+    
+    
+    func test_top_returnNilOnEmptyHeap() {
+        let sut: Heap<Int> = createSUT()
+        XCTAssertNil(sut.top)
+    }
+    
+    fileprivate func createSUT<T>() -> Heap<T> {
+        return Heap<T>()
+    }
+}
